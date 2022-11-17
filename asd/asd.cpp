09b21f76e4d2a057311b06c455e26e0adcbe3266
Никита <@DESKTOP-Q9SPLIR>
2022-11-17 18:34:08 +0300

    Добавьте файлы проекта.

---
+++ asd/asd.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	/*1
+	int a, b, c, d, sum = 0;
+	cin >> a >> b >> c >> d;
+	int arr[4] = { a, b, c, d };
+	for (int i = 0; i < 4; i++) {
+		sum += arr[i];
+		cout << arr[i];
+	}
+	cout << endl << sum;*/
+
+	/*2
+	int arr[4] = {2, 6, 9, 7}, naim = arr[0];
+	for (int i = 0; i < 4; i++) {
+		if (arr[i] % 2 == 0) {
+			if (naim > arr[i])
+				naim = arr[i];
+		}
+	}
+	cout << naim;*/
+
+	int arr[4] = { 6, 0, 3, 6 };
+	for (int i = 0; i < 4; i++) {
+
+	}
+}
